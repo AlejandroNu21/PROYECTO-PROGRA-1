db8b5b9c1df2d85844ac1df957052bd093d8bc9e
AlejandroNu21 <78991134+AlejandroNu21@users.noreply.github.com>
2021-04-22 20:05:16 -0600

    ACTUALIZADO

--- PROYECTO PROGRA 1/PROYECTO PROGRA 1/VISTA/FrmMenu.cs
+++ PROYECTO PROGRA 1/PROYECTO PROGRA 1/VISTA/FrmMenu.cs
@@ -24,21 +24,21 @@ namespace PROYECTO_PROGRA_1.VISTA
 
         private void btnRUsuario_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("<>-- BIENVENIDO--<>");
+            
             FrmRUsuario Donante = new FrmRUsuario();
             Donante.Show();
         }
 
         private void btnCitas_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("<>-- BIENVENIDO --<>");
+            
             FrmAgendarCita Citas = new FrmAgendarCita();
             Citas.Show();
         }
 
         private void btnBuscarDonante_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("<>-- BIENVENIDO --<>");
+           
             FrmBuscarDonante Busqueda = new FrmBuscarDonante();
             Busqueda.Show();
         }
