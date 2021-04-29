14c58a12a2a0ae882555af52b8fb155840f4e948
AlejandroNu21 <78991134+AlejandroNu21@users.noreply.github.com>
2021-04-28 20:16:51 -0600

    Nuevo

---
+++ ProyectoProgramacion/ProyectoProgramacion/VISTA/FrmBuscarDonante.Designer.cs
@@ -0,0 +1,163 @@
+﻿
+namespace ProyectoProgramacion.VISTA
+{
+    partial class FrmBuscarDonante
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDonanteaBuscar = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnEliminarBDonante = new System.Windows.Forms.Button();
+            this.btnSalirBDonante = new System.Windows.Forms.Button();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(298, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingrese Donante a Buscar";
+            // 
+            // txtDonanteaBuscar
+            // 
+            this.txtDonanteaBuscar.Location = new System.Drawing.Point(242, 92);
+            this.txtDonanteaBuscar.Name = "txtDonanteaBuscar";
+            this.txtDonanteaBuscar.Size = new System.Drawing.Size(250, 20);
+            this.txtDonanteaBuscar.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6});
+            this.dataGridView1.Location = new System.Drawing.Point(61, 137);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(600, 150);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // btnEliminarBDonante
+            // 
+            this.btnEliminarBDonante.Location = new System.Drawing.Point(221, 308);
+            this.btnEliminarBDonante.Name = "btnEliminarBDonante";
+            this.btnEliminarBDonante.Size = new System.Drawing.Size(117, 39);
+            this.btnEliminarBDonante.TabIndex = 3;
+            this.btnEliminarBDonante.Text = "ELIMINAR";
+            this.btnEliminarBDonante.UseVisualStyleBackColor = true;
+            // 
+            // btnSalirBDonante
+            // 
+            this.btnSalirBDonante.Location = new System.Drawing.Point(375, 308);
+            this.btnSalirBDonante.Name = "btnSalirBDonante";
+            this.btnSalirBDonante.Size = new System.Drawing.Size(117, 39);
+            this.btnSalirBDonante.TabIndex = 4;
+            this.btnSalirBDonante.Text = "SALIR";
+            this.btnSalirBDonante.UseVisualStyleBackColor = true;
+            this.btnSalirBDonante.Click += new System.EventHandler(this.btnSalirBDonante_Click);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Id";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Nombre Donante";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Direccion";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Telefono";
+            this.Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Edad";
+            this.Column5.Name = "Column5";
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Grupo Sanguineo";
+            this.Column6.Name = "Column6";
+            // 
+            // FrmBuscarDonante
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(712, 399);
+            this.Controls.Add(this.btnSalirBDonante);
+            this.Controls.Add(this.btnEliminarBDonante);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtDonanteaBuscar);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmBuscarDonante";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Buscar Donante";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDonanteaBuscar;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnEliminarBDonante;
+        private System.Windows.Forms.Button btnSalirBDonante;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+    }
+}
---
+++ ProyectoProgramacion/ProyectoProgramacion/VISTA/FrmBuscarDonante.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoProgramacion.VISTA
+{
+    public partial class FrmBuscarDonante : Form
+    {
+        public FrmBuscarDonante()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSalirBDonante_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            FrmMenu frm = new FrmMenu();
+            frm.Show();
+        }
+    }
+}
